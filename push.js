057315c149d666eca6697594c78dd119cdc72ca2
Aime99 <aimegupta@gmail.com>
2020-11-20 16:25:00 +0530

    fix

--- push.js
+++ push.js
@@ -5,5 +5,5 @@ let vapidKeys={
   }
 
   push.setVapidDetails('mailto:aimegupta@gmail.com',vapidKeys.publicKey,vapidKeys.privateKey)
-  let sub={};
+  let sub={"endpoint":"https://fcm.googleapis.com/fcm/send/d1YjLdJDphI:APA91bHkVt2j7Z7a7sMbZKB_on1bCiKxjhnEKU0xB9NpFzWzVolxLssJRoBvVyOMt5d6S--ERrWmerEkRP11xnNIzdkQpQEPobIFj9doEnWISA071N7TBQXvVGp1WPZzr1GiNX4GEDW4","expirationTime":null,"keys":{"p256dh":"BDhhqUonK-RM1Gxx33vB3yHeb_g2hoqRIBy1gF3kff0j2sjBodPezesAVQ7ImpaFp3mETdgQ7ZWsRCxARW0Jg6g","auth":"fYOn7GJKzH1nBxV_JnpJkw"}};
   push.sendNotification(sub,'test message')
